27a1cbfb5aeed13787fd2af675918ee3baaca72c
raink1208 <58714901+raink1208@users.noreply.github.com>
2021-03-24 04:16:44 +0900

    add: permissionの定数

---
+++ src/rain1208/guildsAPI/utils/GuildPermission.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace rain1208\guildsAPI\utils;
+
+
+class GuildPermission
+{
+    const NO_DATA = -1;
+    const OWNER = 0;
+    const admin = 1;
+    const member = 2;
+    const wait = 3;
+}
