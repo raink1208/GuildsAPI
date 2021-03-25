75449162ccf3de6f7d5505479bd05db3493feb9e
raink1208 <58714901+raink1208@users.noreply.github.com>
2021-03-26 07:08:14 +0900

    add: readonlyの同期DBを追加

--- resources/sqls/sqlite.sql
+++ resources/sqls/sqlite.sql
@@ -12,5 +12,20 @@ CREATE TABLE IF NOT EXISTS players(id TEXT PRIMARY KEY, guild_id INTEGER NOT NUL
         -- #}
     -- #}
 
+    -- #{ guild
+
+        -- #{ create
+
+INSERT INTO guilds (id, name, level, exp) VALUES (:id, :name, :level, :exp);
+
+        -- #}
+
+        -- #{ save
+
+UPDATE guilds SET level=:level, exp=:exp WHERE id=:guild_id;
+
+        -- #}
+
+    -- #}
 
 -- #}
--- src/rain1208/guildsAPI/utils/SQLiteDatabase.php
+++ src/rain1208/guildsAPI/utils/SQLiteDatabase.php
@@ -7,9 +7,12 @@ namespace rain1208\guildsAPI\utils;
 use poggit\libasynql\DataConnector;
 use poggit\libasynql\libasynql;
 use rain1208\guildsAPI\Main;
+use rain1208\guildsAPI\models\GuildId;
+use SQLite3;
 
 class SQLiteDatabase
 {
+    private SQLite3 $syncDB;
     private DataConnector $db;
 
     public function __construct(Main $plugin)
@@ -24,6 +27,48 @@ class SQLiteDatabase
 
         $this->db->executeGeneric("guildsql.init.guilds");
         $this->db->executeGeneric("guildsql.init.players");
+
+        $file = $plugin->getDataFolder().$plugin->getConfig()->get("database")["sqlite"]["file"];
+        $this->syncDB = new SQLite3($file, SQLITE3_OPEN_READONLY);
+    }
+
+    public function getAllGuildData(): array
+    {
+        $result = [];
+
+        $stmt = $this->syncDB->query("SELECT guild_id, name, level, exp, p.id FROM guilds inner join players p on guilds.id = p.guild_id WHERE permission=0");
+
+        while ($res = $stmt->fetchArray(SQLITE3_ASSOC)) {
+            $result[] = $res;
+        }
+
+        return $result;
+    }
+
+
+    /**
+     * @param int|GuildId $id
+     * @return array
+     */
+    public function getGuildMember($id): array
+    {
+        if ($id instanceof GuildId) {
+            $id = $id->getValue();
+        }
+
+        $stmt = $this->syncDB->prepare("SELECT id, permission FROM players WHERE guild_id=:guild_id");
+
+        $stmt->bindValue(":guild_id", $id);
+
+        $stmt = $stmt->execute();
+
+        $result = [[],[],[],[]];
+
+        while ($res = $stmt->fetchArray(SQLITE3_ASSOC)) {
+            $result[$res["permission"]][] = $res["id"];
+        }
+
+        return $result;
     }
 
     public function close()
@@ -33,5 +78,7 @@ class SQLiteDatabase
         if (isset($this->db)) {
             $this->db->close();
         }
+
+        $this->syncDB->close();
     }
 }
