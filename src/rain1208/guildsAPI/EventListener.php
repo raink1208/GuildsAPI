7792d84ce25deb133d68638568968bc68b99b426
raink1208 <58714901+raink1208@users.noreply.github.com>
2021-03-25 23:49:07 +0900

    update: プレイヤーが退出時にデータを保存

--- src/rain1208/guildsAPI/EventListener.php
+++ src/rain1208/guildsAPI/EventListener.php
@@ -6,11 +6,19 @@ namespace rain1208\guildsAPI;
 
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerQuitEvent;
 
 class EventListener implements Listener
 {
     public function onJoin(PlayerJoinEvent $event)
     {
         Main::getInstance()->getGuildPlayerManager()->loadPlayer($event->getPlayer()->getName());
+    }
+
+    public function onQuit(PlayerQuitEvent $event)
+    {
+        $player = Main::getInstance()->getGuildPlayerManager()->getGuildPlayer($event->getPlayer()->getName());
+
+        Main::getInstance()->getDatabase()->savePlayerData($player);
     }
 }
