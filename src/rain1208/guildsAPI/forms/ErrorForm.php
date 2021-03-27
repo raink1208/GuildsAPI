5599b8ee6b9dd51957b687b4770a212b1edf330e
raink1208 <58714901+raink1208@users.noreply.github.com>
2021-03-28 01:10:43 +0900

    add: エラー時に表示するFormを追加

---
+++ src/rain1208/guildsAPI/forms/ErrorForm.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace rain1208\guildsAPI\forms;
+
+
+use pocketmine\form\Form;
+use pocketmine\Player;
+use rain1208\guildsAPI\forms\addons\AbstractModalForm;
+
+class ErrorForm extends AbstractModalForm
+{
+    private Form $back;
+
+    public function __construct(string $text,Form $back)
+    {
+        $this->back = $back;
+
+        $title = "Error Form";
+        $yesButtonText = "戻る";
+        $noButtonText = "やめる";
+        parent::__construct($title, $text, $yesButtonText, $noButtonText);
+    }
+
+    public function submit(Player $player, bool $bool): void
+    {
+        if ($bool) {
+            $player->sendForm($this->back);
+        }
+    }
+}
