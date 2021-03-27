152c984012b827888448c6072505f280e51442f9
rain1208 <kkouga1208@gmail.com>
2021-03-28 07:46:55 +0900

    add: ギルド削除Formを追加

---
+++ src/rain1208/guildsAPI/forms/guilds/delete/GuildDeleteForm.php
@@ -0,0 +1,49 @@
+<?php
+
+
+namespace rain1208\guildsAPI\forms\guilds\delete;
+
+
+use dktapps\pmforms\CustomFormResponse;
+use dktapps\pmforms\element\Input;
+use dktapps\pmforms\element\Label;
+use pocketmine\Player;
+use rain1208\guildsAPI\forms\addons\AbstractCustomForm;
+use rain1208\guildsAPI\forms\ErrorForm;
+use rain1208\guildsAPI\guilds\Guild;
+use rain1208\guildsAPI\Main;
+
+class GuildDeleteForm extends AbstractCustomForm
+{
+    private Guild $guild;
+
+    public function __construct(Guild $guild)
+    {
+        $this->guild = $guild;
+
+        $title = "";
+
+        $elements = [
+            new Label("label", $guild->getGuildInfoString()),
+            new Input("Name", "ギルド名を入力してください(誤削除防止のため)")
+        ];
+
+        parent::__construct($title, $elements);
+    }
+
+    public function submit(Player $player, CustomFormResponse $response): void
+    {
+        $inputName = $response->getString("Name");
+        $guildPlayer = Main::getInstance()->getGuildPlayerManager()->getGuildPlayer($player->getName());
+
+        if (!$guildPlayer->getGuildId()->equals($this->guild->getGuildId())) {
+            $player->sendForm(new ErrorForm(""));
+            return;
+        }
+
+        if ($this->guild->getName() !== $inputName) {
+            $player->sendForm(new ErrorForm("入力したギルド名が一致しませんでした", $this));
+            return;
+        }
+    }
+}
